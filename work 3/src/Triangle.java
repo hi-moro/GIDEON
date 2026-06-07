8d0cc1c536fd2cc1b8b2e77ca6e81c76fbdb7399
Gideon Naah <gideonnaah815@gmail.com>
2026-06-07 21:05:04 +0000

    Updated personal information program

---
+++ work 3/src/Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle {
+    public static void main(String[] args) {
+
+        System.out.println("             A                                ");
+        System.out.println("            AAA                               ");
+        System.out.println("          AAAAAAA                                 ");
+        System.out.println("        AAAAAAAAAAA                           ");
+        System.out.println("      AAAAAAAAAAAAAAA ");
+        System.out.println("    AAAAAAAAAAAAAAAAAAA                     ");
+        System.out.println("   AAAAAAAAAAAAAAAAAAAAAA                          ");
+
+    }
+
+
+
+
+
+}
---
+++ work 3/src/tableandchair.java
@@ -0,0 +1,19 @@
+public class tableandchair {
+    public static void main(String[] args) {
+
+        System.out.println("x                                             x");
+        System.out.println("x                                             x");
+        System.out.println("x                                             x");
+        System.out.println("x                                             x");
+        System.out.println("xxxxxxxxxx     xxxxxxxxxxx          xxxxxxxxxxx");
+        System.out.println("x        x     x         x          x         x");
+        System.out.println("x        x     x         x          x         x");
+        System.out.println("x        x     x         x          x         x");
+        System.out.println("x        x     x         x          x         x");
+    }
+
+
+
+
+
+}
